a5e4b4e9efaa43c9ed43a4ed1cc3ad7875745212
江寒 <3685634009@qq.com>
2026-07-22 04:29:42 +0800

    将app版本从3.0.0升级至3.1.0 新增一键跳过设计

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 33
         targetSdk = 37
         versionCode = 14
-        versionName = "3.0.0"
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
