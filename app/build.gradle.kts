d5ed962f727d7277cdb89356011590e9b16f49ae
江寒 <3685634009@qq.com>
2026-07-24 03:22:28 +0800

    build(app): 更新版本号至3.2.3
    将app模块的版本名称从3.2.2调整为3.2.3，完成版本迭代更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 33
         targetSdk = 37
         versionCode = 15
-        versionName = "3.2.2"
+        versionName = "3.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
