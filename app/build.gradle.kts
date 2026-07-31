1cb7920c05beffaa76f74aa18cfa19e937d41a48
江寒 <3685634009@qq.com>
2026-07-31 15:23:18 +0800

    build(app): 更新版本号至4.4.4
    将应用的版本名称从4.4.3调整为4.4.4，完成版本迭代更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 33
         targetSdk = 37
         versionCode = 15
-        versionName = "4.4.3"
+        versionName = "4.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
