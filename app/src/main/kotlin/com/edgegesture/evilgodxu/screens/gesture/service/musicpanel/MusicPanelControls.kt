58e70a7415c4f18f1de8811f12c01787258ca6c7
江寒 <3685634009@qq.com>
2026-08-06 20:20:22 +0800

    优化音乐面板按钮布局

--- app/src/main/kotlin/com/edgegesture/evilgodxu/screens/gesture/service/musicpanel/MusicPanelControls.kt
+++ app/src/main/kotlin/com/edgegesture/evilgodxu/screens/gesture/service/musicpanel/MusicPanelControls.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -122,7 +123,8 @@ internal fun ControlBar(
                 shape = CircleShape,
                 color = androidx.compose.ui.graphics.Color.Transparent,
                 modifier = Modifier
-                    .align(Alignment.CenterStart)
+                    .align(Alignment.Center)
+                    .offset(x = (-110).dp)
                     .size(32.dp),
                 onClick = onLyricsRefreshClick
             ) {
